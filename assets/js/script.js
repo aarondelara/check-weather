4c0a511c22d738b6148455ebb3455aa338b6c71f
Aaron DeLara <aarondelara64@gmail.com>
2023-07-12 15:43:27 -0700

    finished displaying forecast and redid js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,86 @@
 
-var searchButton = document.querySelector('#search-button')
-var inputCity = document.querySelector('#city-name')
+var searchButton = document.querySelector('#search-button');
+var inputCity = document.querySelector('#city-name');
+var cityContainer = document.querySelector('#current-city');
+var forecastContainer = document.querySelector('#forecast');
 
 function getLocation(city) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=a8469e3590068e1ffd438d0a50ae7be8`)
         .then(function (response) {
-            return response.json()
+            return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            var lat = data[0].lat
-            var lon = data[0].lon
-            getCurrentWeather(lat, lon)
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            getCurrentWeather(lat, lon);
+            getForecastWeather(lat, lon);
         })
-    console.log('hello')
 }
 
 function getCurrentWeather(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a8469e3590068e1ffd438d0a50ae7be8&units=imperial`)
         .then(function (response) {
-            return response.json()
+            return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            displayCurrentWeather(data)
+            var section = convertDataToHTML(data, true);
+            cityContainer.appendChild(section);
         })
 }
 
-function displayCurrentWeather(data) {
-    var currentCity = document.querySelector('#city')
-    var icon = document.querySelector('#weather-icon')
-    var currentTemp = document.querySelector('#temp')
-    var currentWind = document.querySelector('#wind')
-    var currentHumidity = document.querySelector('#humidity')
-
-    currentCity.textContent = data.name
-    icon.setAttribute(`src`, `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-    currentTemp.textContent = "Temp: " + data.main.temp + "\u00B0" + "F"
-    currentWind.textContent = "Wind: " + data.wind.speed + " MPH"
-    currentHumidity.textContent = "Humidity: " + data.main.humidity + "%"
+function convertDataToHTML(data, cityName) {
+    var section = document.createElement('section');
+    var time = document.createElement('h2');
+    var icon = document.createElement('img');
+    var temp = document.createElement('p');
+    var wind = document.createElement('p');
+    var humidity = document.createElement('p');
+    
+    section.className = 'city';
+    var t = new Date(data.dt*1000);
+    time.textContent = `${t.getMonth()}/${t.getDate()}/${t.getFullYear()} ${t.getHours().toString().padStart(2,`0`)}:${t.getMinutes().toString().padStart(2,`0`)} `;
+    icon.setAttribute(`src`, `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    temp.textContent = "Temp: " + data.main.temp + "\u00B0" + "F";
+    wind.textContent = "Wind: " + data.wind.speed + " MPH";
+    humidity.textContent = "Humidity: " + data.main.humidity + "%";
+    if (cityName) {
+        var city = document.createElement('h2');
+        city.textContent = data.name;
+        section.appendChild(city);
+    }
+    section.appendChild(time);
+    section.appendChild(icon);
+    section.appendChild(temp);
+    section.appendChild(wind);
+    section.appendChild(humidity);
+    return section;
 }
 
-function getForecastWeather(lat,lon) {
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a8469e3590068e1ffd438d0a50ae7be8&units=imperial`)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data)
-        displayCurrentWeather(data)
-    })
-}
 
+function getForecastWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a8469e3590068e1ffd438d0a50ae7be8&units=imperial`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.list.length; i++) {
+                var forecastData = data.list[i];
+                var section = convertDataToHTML(forecastData);
+                forecastContainer.appendChild(section);
+            }
+
+        })
+}
 
 
 searchButton.addEventListener('click', function () {
-    var searchedCity = inputCity.value
-    getLocation(searchedCity)
+    var searchedCity = inputCity.value;
+    getLocation(searchedCity);
 })
 
 inputCity.addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         event.preventDefault();
-        document.querySelector('#search-button').click();
+        searchButton.click();
     }
 });
-
-//var currentDate = newDate();
-//var formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
-//console.log(formattedDate);
